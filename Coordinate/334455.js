79c71b327c406955f48e332cf345c5671a3facb8
Octopus142 <blackfairyforma726@gmail.com>
2024-08-28 00:45:19 +0800

    Update 334455.js

--- Coordinate/334455.js
+++ Coordinate/334455.js
@@ -1,6 +1,6 @@
 import 'Triangle3.vbs'
 import 'Square4.vbs'
 import 'pentagon5.vbs'
-export default Trianglefuncrion();{
+export default Trianglefunction();{
     Triangle3 ^ 2 + Square4 ^ 2 == pentagon5 ^ 2 
 }
