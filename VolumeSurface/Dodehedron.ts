9559d7a6c9a870adeac5d5b67452a35d47fd536d
Octopus142 <blackfairyforma726@gmail.com>
2024-08-28 08:06:39 +0800

    Update Dodehedron.ts

--- VolumeSurface/Dodehedron.ts
+++ VolumeSurface/Dodehedron.ts
@@ -4,5 +4,3 @@ function CalculateVolume(sideLength: number): number {
    function CalculateSurfaceArea(sideLength: number): number {
     return 15 * Math.sqrt(3) * Math.pow(sideLength, 2);
    }
-   console.log(calculateVolume(NaN)); // 117.85
-   console.log(calculateSurfaceArea(NaN)); // 105
