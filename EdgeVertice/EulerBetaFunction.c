2a79c8ed2b1f109534e42d740a14c54b44783b9d
Octopus142 <blackfairyforma726@gmail.com>
2024-08-28 08:28:16 +0800

    Delete EdgeVertice/EulerBetaFunction.c

--- EdgeVertice/EulerBetaFunction.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-int main() {
-    double angle = 1.0; // 示例角度值，单位为弧度
-    double sinValue = sin(angle);
-    printf("The sine of %.2f radians is %.2f\n", angle, sinValue);
-    return 0;
-}
