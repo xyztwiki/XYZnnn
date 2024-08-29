69f68bcb831a1952cd59fbd879c41d0a7e218e02
Octopus142 <blackfairyforma726@gmail.com>
2024-08-29 13:23:38 +0800

    Update SphereCoordinate.vb

--- Coordinate/SphereCoordinate.vb
+++ Coordinate/SphereCoordinate.vb
@@ -1,21 +1,9 @@
 Imports System.Math
 Imports "Triangle3.c"
-REM 定义球面坐标变量
 DIM r AS DOUBLE
 DIM theta AS DOUBLE
 DIM phi AS DOUBLE
 
-REM 设置球面坐标值
-r = 5 ' 示例半径
-theta = 0.5 * PI ' 示例极角
-phi = 0.75 * PI ' 示例方位角
-
-REM 计算笛卡尔坐标
-X = r * SIN(theta) * COS(phi)
-Y = r * SIN(theta) * SIN(phi)
-Z = r * COS(theta)
-
-REM 输出笛卡尔坐标
-PRINT "X: "; X
-PRINT "Y: "; Y
-PRINT "Z: "; Z
+Byval X = r * SIN(theta) * COS(phi)
+Byval Y = r * SIN(theta) * SIN(phi)
+Byval Z = r * COS(theta)
