8125c92edd4c6e90d92397730444f4e1b768da0b
Octopus142 <blackfairyforma726@gmail.com>
2024-08-29 13:33:04 +0800

    Update RectangularCoordinate.vb

--- Coordinate/RectangularCoordinate.vb
+++ Coordinate/RectangularCoordinate.vb
@@ -3,7 +3,6 @@ Imports "Square4.c"
 Dim X AS SINGLE
 Dim Y AS SINGLE
 Dim Z AS SINGLE
-' Convert rectangular coordinates to spherical coordinates
 Function RectangularToSphere(ByVal X , ByVal Y , ByVal Z ) As SphereCoordinate
     Dim r As Single
     Dim theta As Single
@@ -12,5 +11,5 @@ Function RectangularToSphere(ByVal X , ByVal Y , ByVal Z ) As SphereCoordinate
     r = Sqrt(X * X + Y * Y + Z * Z)
     theta = Atan2(Y, X)
     phi = Atan2(Sqr(X * X + Y * Y), Z)
-    
-    RectangularToSpherical.R = r
+
+End Function
