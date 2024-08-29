97b6beb253f7a062baa1ad44adba4f862e1be8c8
Octopus142 <blackfairyforma726@gmail.com>
2024-08-30 04:21:13 +0800

    Update Octahedron.ts

--- VolumeSurface/Octahedron.ts
+++ VolumeSurface/Octahedron.ts
@@ -1,12 +1,8 @@
-// 定义一个函数来计算正八面体的体积
-function calculateVolume(Edge: number): number {
+// 正八面体
+function Volume(Edge: number){
     return (2 * Math.sqrt(2)) / 3 * Math.pow(Edge, 3);
-   }
-   // 定义一个函数来计算正八面体的表面积
-   function calculateSurfaceArea(Edge: number): number {
+}
+function Surface(Edge: number){
     return 2 * Math.sqrt(3) * Math.pow(Edge, 2);
-   }
-   // 使用示例
-   const Edge = NaN; // 正八面体的边长
-   const Volume = calculateVolume(Edge);
-   const SurfaceArea = calculateSurfaceArea(Edge);
+}
+   const Edge = NaN; // 正八面体
