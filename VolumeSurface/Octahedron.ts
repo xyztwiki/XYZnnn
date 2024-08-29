7fa33c441bf6712681b1ba155d10e1b95289a661
Octopus142 <blackfairyforma726@gmail.com>
2024-08-30 04:22:18 +0800

    Update Octahedron.ts

--- VolumeSurface/Octahedron.ts
+++ VolumeSurface/Octahedron.ts
@@ -2,7 +2,7 @@
 function Volume(Edge: number){
     return (2 * Math.sqrt(2)) / 3 * Math.pow(Edge, 3);
 }
-function Surface(Edge: number){
+function SurfaceArea(Edge: number){
     return 2 * Math.sqrt(3) * Math.pow(Edge, 2);
 }
    const Edge = NaN;
