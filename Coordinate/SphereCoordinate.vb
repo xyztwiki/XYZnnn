2ef24c6ddb90869065f6e91cd18eafce8f27e844
Octopus142 <blackfairyforma726@gmail.com>
2024-08-29 13:39:20 +0800

    Update SphereCoordinate.vb

--- Coordinate/SphereCoordinate.vb
+++ Coordinate/SphereCoordinate.vb
@@ -3,7 +3,12 @@ Imports "Triangle3.c"
 DIM r AS DOUBLE
 DIM theta AS DOUBLE
 DIM phi AS DOUBLE
-
+Function Sphere(ByVal X , ByVal Y , ByVal Z ) As SphereCoordinate
+    
+    r = Sqrt(X * X + Y * Y + Z * Z)
+    theta = Atan2(Y, X)
+    phi = Atan2(Sqr(X * X + Y * Y), Z)
+  
 Byval X = r * SIN(theta) * COS(phi)
 Byval Y = r * SIN(theta) * SIN(phi)
 Byval Z = r * COS(theta)
