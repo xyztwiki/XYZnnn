3efd538fc6bf75dd5ff149ecfd5724339f76b65d
Octopus142 <blackfairyforma726@gmail.com>
2024-08-30 04:20:03 +0800

    Update tetrahedron.ts

--- VolumeSurface/tetrahedron.ts
+++ VolumeSurface/tetrahedron.ts
@@ -2,10 +2,10 @@ class Tetrahedron {
     constructor(edge: number) {
         this.edge = edge;
     }
-    get volume(): number {
-        return Math.sqrt(2) / 12 * this.edge ** 3;
+    volume(){
+        return Math.sqrt(2) / 12 * this.edge * 3;
     }
-    get surfaceArea(): number {
-        return Math.sqrt(3) * this.edge ** 2;
+    surfaceArea(){
+        return Math.sqrt(3) * this.edge * 2;
     }
 }
