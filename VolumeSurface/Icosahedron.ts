e41ea8d66a028bbf404a2bcb68d23e18f7131260
Octopus142 <blackfairyforma726@gmail.com>
2024-08-30 04:22:07 +0800

    Update Icosahedron.ts

--- VolumeSurface/Icosahedron.ts
+++ VolumeSurface/Icosahedron.ts
@@ -1,6 +1,3 @@
-// 定义正二十面体的边长
-const edge: number = NaN; // 示例边长
-// 计算体积
-export const volume = (5 / 12) * (3 + Math.sqrt(5)) * Math.pow(edge, 3);
-// 计算表面积
-export const surfaceArea = 5 * Math.sqrt(3) * Math.pow(edge, 2);
+//二十面体
+const volume = (5 / 12) * (3 + Math.sqrt(5)) * Math.pow(edge, 3);
+const surfaceArea = 5 * Math.sqrt(3) * Math.pow(edge, 2);
