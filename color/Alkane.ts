2ccd3e5a9e02131ad2bec4ac8032bed3c16cdafd
Octopus142 <blackfairyforma726@gmail.com>
2024-08-30 04:14:51 +0800

    Update Alkane.ts

--- color/Alkane.ts
+++ color/Alkane.ts
@@ -6,16 +6,16 @@ class C1H4 implements Alkane {
     readonly carbonCount: number = 1;
     readonly hydrogenCount: number = 4;
 }
-class C2H6 implements Alkane {
-    readonly carbonCount: number = 2;
-    readonly hydrogenCount: number = 6;
-}
-class C3H8 implements Alkane {
-    readonly carbonCount: number = 3;
+class C5H8 implements Alkane {
+    readonly carbonCount: number = 5;
     readonly hydrogenCount: number = 8;
+}
+class C13H24 implements Alkane {
+    readonly carbonCount: number = 13;
+    readonly hydrogenCount: number = 24;
 }
 void function *generateAlkyne(){
     yield new C1H4()
-    yield new C2H6()
-    yield new C3H8()
+    yield new C5H8()
+    yield new C13H24()
 }
