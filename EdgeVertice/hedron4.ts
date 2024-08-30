c4dcc7db0871ddb2bb4f29d58504da81249ae7bb
Octopus142 <blackfairyforma726@gmail.com>
2024-08-30 12:56:30 +0800

    Update hedron4.ts

--- EdgeVertice/hedron4.ts
+++ EdgeVertice/hedron4.ts
@@ -1,10 +1,11 @@
-class hedron4(){
-  constructor{x:number,y:number,z:number}{
-  function edge{}{
-     return [x:0,y:0,z:0}
+class hedron4(){  
+    constructor{x:number,y:number,z:number}{
+function edge(){
+    return [
             {x:1,y:0,z:0}
             {x:0,y:1,z:0}
-            {x:0,y:0,z:Math.sqrt{2}/2}
-       ]
-    }
+            {x:0,y:0,z:1}
+            {x:0,y:0,z:Math.sqrt(2)/2}
+            ]
+       }
 }
