1570c1c2d9cf0bf6897cea908c632899c002a7ab
Octopus142 <blackfairyforma726@gmail.com>
2024-08-30 13:09:57 +0800

    Update hedron10.ts

--- EdgeVertice/hedron10.ts
+++ EdgeVertice/hedron10.ts
@@ -8,16 +8,15 @@ class Point {
         this.z = z;
     }
 }
-export const vertices: Point[] = [
+const vertices: Point[] = [
     new Point(0, 0, 1),
     new Point(0, -Math.sqrt(3)/2, 0),
     new Point(0, 0, Math.sqrt(3)/2),
     new Point(1, 0, 0),
 ]
-export const faces: Point[][] = [
+const faces: Point[][] = [
     [vertices[0], vertices[1], vertices[2]],
     [vertices[0], vertices[2], vertices[3]],
     [vertices[0], vertices[1], vertices[3]],
     [vertices[1], vertices[2], vertices[3]],
 ]
-console.log(vertices,faces)
