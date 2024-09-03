005dd1ca88d336f0e9bc2e5aaa17cc9d7b74474f
Octopus142 <blackfairyforma726@gmail.com>
2024-09-03 15:10:51 +0800

    Update hedron20.ts

--- EdgeVertice/hedron20.ts
+++ EdgeVertice/hedron20.ts
@@ -1,14 +1,1 @@
-function IcosahedronEdgeVertice(radius: number) {
-    const phi = (1 + Math.sqrt(5)) / 2;
-    const edge = 2 * radius * Math.sqrt(5) / 10;
-    const vertices: [][][] = {};
-    for (let i = 0; i < 12; ++i) {
-        vertices.push([radius * Math.sin(theta) * Math.cos(phi), 
-                       radius * Math.sin(theta) * Math.sin(phi), 
-                       radius * Math.cos(theta)]);
-    }
-    const edges: [][][] = [];
-    for (let i = 0; i < 12; ++i) {
-        edges.push([i, (i + 1) % 12]);
-    }
-}
+
