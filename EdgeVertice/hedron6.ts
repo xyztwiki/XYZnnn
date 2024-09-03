2b727480f06409204c9cbc753dc401d93c836e83
Octopus142 <blackfairyforma726@gmail.com>
2024-09-03 14:51:54 +0800

    Update hedron6.ts

--- EdgeVertice/hedron6.ts
+++ EdgeVertice/hedron6.ts
@@ -8,21 +8,21 @@ class Point {
         this.z = z;
     }
 }
-const vertice: Point[] = [
-    new Point(1, 1, 1),    // 0: front top right
-    new Point(-1, 1, 1),   // 1: front top left
-    new Point(-1, -1, 1),  // 2: front bottom left
-    new Point(1, -1, 1),   // 3: front bottom right
-    new Point(1, 1, -1),   // 4: back top right
-    new Point(-1, 1, -1),  // 5: back top left
-    new Point(-1, -1, -1), // 6: back bottom left
-    new Point(1, -1, -1)   // 7: back bottom right
+const Point[] = [
+     Point1(1, 1, 1),    // 0: front top right
+     Point2(-1, 1, 1),   // 1: front top left
+     Point3(-1, -1, 1),  // 2: front bottom left
+     Point4(1, -1, 1),   // 3: front bottom right
+     Point5(1, 1, -1),   // 4: back top right
+     Point6(-1, 1, -1),  // 5: back top left
+     Point7(-1, -1, -1), // 6: back bottom left
+     Point8(1, -1, -1)   // 7: back bottom right
 ];
 const faces: Point[][] = [
-    [vertice[0], vertice[1], vertice[2], vertice[3]], // Front face
-    [vertice[4], vertice[5], vertice[6], vertice[7]], // Back face
-    [vertice[0], vertice[4], vertice[7], vertice[3]], // Right face
-    [vertice[1], vertice[5], vertice[6], vertice[2]], // Left face
-    [vertice[0], vertice[1], vertice[5], vertice[4]], // Top face
-    [vertice[3], vertice[2], vertice[6], vertice[7]]  // Bottom face
+    [Point[0], Point[1], Point[2], Point[3]], // Front face
+    [Point[4], Point[5], Point[6], Point[7]], // Back face
+    [Point[0], Point[4], Point[7], Point[3]], // Right face
+    [Point[1], Point[5], Point[6], Point[2]], // Left face
+    [Point[0], Point[1], Point[5], Point[4]], // Top face
+    [Point[3], Point[2], Point[6], Point[7]]  // Bottom face
 ];
