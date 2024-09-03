df1086a27692ec8756d535dd444835345e107a27
Octopus142 <blackfairyforma726@gmail.com>
2024-09-03 15:30:03 +0800

    Update Dodehedron.ts

--- VolumeSurface/Dodehedron.ts
+++ VolumeSurface/Dodehedron.ts
@@ -1,6 +1,6 @@
-function CalculateVolume(edge: number){
+function CalculateVolume(Edge: number){
     return (19 / 12) * Math.pow(edge, 3);
    }
-   function CalculateSurfaceArea(edge: number){
+   function CalculateSurfaceArea(Edge: number){
     return 15 * Math.sqrt(3) * Math.pow(edge, 2);
    }
