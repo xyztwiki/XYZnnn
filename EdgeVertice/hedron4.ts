7855f0b69d5d346fd048c01c3dd0d8ae9b1a01e4
Octopus142 <blackfairyforma726@gmail.com>
2024-09-03 14:29:24 +0800

    Update hedron4.ts

--- EdgeVertice/hedron4.ts
+++ EdgeVertice/hedron4.ts
@@ -1,11 +1,14 @@
-class hedron4(){  
-    constructor{x:number,y:number,z:number}{
-function edge(){
-    return [
-            {x:1,y:0,z:0}
-            {x:0,y:1,z:0}
-            {x:0,y:0,z:1}
-            {x:0,y:0,z:Math.sqrt(2)/2}
-            ]
-       }
+abstract class tetrahedron(){
+    constructor(r:string, theta:number, phi:number){
+        const x = this.r * Math.sin(this.theta) * Math.cos(this.phi);
+        const y = this.r * Math.sin(this.theta) * Math.sin(this.phi);
+        const z = this.r * Math.cos(this.phi);
+    }
+    const tetrahedronAngle = Math.acos(-1/3);
+        return [
+            new SphericalPoint(radius, 0, 0),
+            new SphericalPoint(radius, 0, tetrahedronAngle),
+            new SphericalPoint(radius, 2 * Math.PI / 3, tetrahedronAngle),
+            new SphericalPoint(radius, 4 * Math.PI / 3, tetrahedronAngle)
+        ];
 }
