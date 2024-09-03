3b5b4f1db800496ea358dc2e20ada04fb8113114
Octopus142 <blackfairyforma726@gmail.com>
2024-09-03 14:14:28 +0800

    Update and rename hedron10.ts to hedron6.ts

--- EdgeVertice/hedron6.ts
+++ EdgeVertice/hedron6.ts
@@ -13,10 +13,14 @@ const vertices: Point[] = [
     new Point(0, -Math.sqrt(3)/2, 0),
     new Point(0, 0, Math.sqrt(3)/2),
     new Point(1, 0, 0),
+    new Point(),
+    new Point(),
 ]
 const faces: Point[][] = [
     [vertices[0], vertices[1], vertices[2]],
     [vertices[0], vertices[2], vertices[3]],
+    [vertices[0], vertices[1], vertices[3]],
+    [vertices[1], vertices[2], vertices[3]],
     [vertices[0], vertices[1], vertices[3]],
     [vertices[1], vertices[2], vertices[3]],
 ]
